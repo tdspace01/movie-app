330d393b91493e00b9d2ab5c90d0ca2d94ff5687
tdspace01 <tdspacein@gmail.com>
2026-06-13 09:25:54 +0400

    Core Module Implementation
    # Conflicts:
    #	app/build.gradle.kts
    #	core/designsystem/build.gradle.kts
    #	core/navigation/build.gradle.kts
    #	core/network/build.gradle.kts
    #	core/ui/build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -50,5 +50,13 @@ gradlePlugin {
             id = "convention.kotlin.library"
             implementationClass = "KotlinLibraryConventionPlugin"
         }
+        register("kotlinLibrary"){
+            id = "convention.kotlin.library"
+            implementationClass = "KotlinLibraryConventionPlugin"
+        }
+        register("test"){
+            id = "convention.test"
+            implementationClass = "TestConventionPlugin"
+        }
     }
 }
