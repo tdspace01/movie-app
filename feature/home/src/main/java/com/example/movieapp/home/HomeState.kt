282533a9968f7a0bda2efd8df82e27759967aa39
tdspace01 <tdspacein@gmail.com>
2026-06-24 16:36:19 +0400

    Removed not needed files

--- feature/home/src/main/java/com/example/movieapp/home/HomeState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieapp.home
-
-data class HomeState(
-    val id:Int
-)
--- feature/moviedetail/src/main/java/com/example/movieapp/moviedetail/MovieDetailState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieapp.moviedetail
-
-data class MovieDetailState(
-    val id:Int
-)
