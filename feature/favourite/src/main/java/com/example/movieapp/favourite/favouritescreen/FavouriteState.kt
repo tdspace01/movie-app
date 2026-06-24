ac34a120b052bfbc0e44609d2e97b1c2496bb35a
tdspace01 <tdspacein@gmail.com>
2026-06-24 16:43:11 +0400

    Removed not needed files

--- feature/favourite/src/main/java/com/example/movieapp/favourite/favouritescreen/FavouriteState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieapp.favourite.favouritescreen
-
-data class FavouriteState(
-    val id:Int
-)
--- feature/moviedetail/src/main/java/com/example/movieapp/moviedetail/moviedetailscreen/MovieDetailState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieapp.moviedetail.moviedetailscreen
-
-data class MovieDetailState(
-    val id:Int
-)
